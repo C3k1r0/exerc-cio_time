8312a514733654a40b63b0908902e39a1c506702
ramon <ramonzchaves@gmail.com>
2024-10-08 19:30:40 -0300

    primeiro comit dos times

---
+++ index.js
@@ -0,0 +1,12 @@
+function validainput(){
+  const time = document.querySelector("#qualquercoisa").value
+  if (time == "flamengo"){
+    window.location.href='https://www.youtube.com/watch?v=-LejxGwfbH8'
+  } else if (time == "vasco"){
+    window.location.href='https://www.youtube.com/watch?v=Y7S96nA7NE4'
+  } else {
+    window.alert("goooooool")
+  }
+  
+}
+
